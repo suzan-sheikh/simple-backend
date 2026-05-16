c918381d2df90b97cbdf24a5f693ab4503ac127a
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-16 15:13:48 +0600

    single product get successfull

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { readProduct } from "../service/product.service";
+import type { IProducts } from "../types/product.types";
 
 export const productController = (
   req: IncomingMessage,
@@ -7,13 +8,23 @@ export const productController = (
 ) => {
   const url = req.url;
   const method = req.method;
-
   const products = readProduct();
+
+  const urlParts = url?.split("/");
+
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
 
   if (url === "/products" && method === "GET") {
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
-      JSON.stringify({ message: "{product get success}", data: products }),
+      JSON.stringify({ message: "product get success", data: products }),
     );
+  } else if (method === "GET" && id !== null) {
+    const products = readProduct();
+    const product = products.find((p:IProducts) => p.id === id)
+    res.writeHead(200, {"content-type": "application/json"})
+    res.end(JSON.stringify({message: "single product get success", data: product}))
+    
   }
 };
---
+++ src/types/product.types.ts
@@ -0,0 +1,11 @@
+export interface IProducts {
+  id: number;
+  name: string;
+  category: string;
+  brand: string;
+  price: number;
+  currency: string;
+  stock: number;
+  rating: number;
+  sku: string;
+}
