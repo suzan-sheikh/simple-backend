8a4f325c2fa562cea6cb5ed3fa53de654396eae2
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-16 11:24:15 +0600

    change route

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,15 @@ const server: Server = createServer(
     const method = req.method;
 
     if (url === "/" && method === "GET") {
-      res.writeHead(200, { "content-type": "text/plain" });
-      res.end(JSON.stringify({ message: "This is root route" }));
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "this is root rout" }));
     } else {
-      res.writeHead(404, { "content-type": "text/plain" });
+      res.writeHead(404, { "content-type": "application/json" });
       res.end(JSON.stringify({ message: "route not found" }));
     }
   },
 );
 
-server.listen(3000, () => {
-  console.log("the server is running on port 3000");
+server.listen(5000, () => {
+  console.log("Server is Running port 5000");
 });
